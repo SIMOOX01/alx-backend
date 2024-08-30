621e1870d46adb097a13d7b99ae42da2f89664a2
SIMOOX01 <essalmisimo0@gmail.com>
2024-08-30 10:15:23 +0000

    dd

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -43,4 +43,6 @@ class Server:
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Returns a tuple containing a start and end index.
     """
-    return ((page - 1) * page_size, page * page_size)
+    return ((page - 1) * page_size, page * page_size
+)
+
