2b00ccc90731a611c600983311cd84c19ec79631
SIMOOX01 <essalmisimo0@gmail.com>
2024-08-31 19:28:09 +0000

    2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" 1. Simple pagination
+""" 2. Hypermedia pagination
 """
 
 import csv
-from typing import List, Tuple
+import math
+from typing import Dict, List, Tuple, Any
 
 
 class Server:
@@ -22,25 +23,36 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10):
         """ Finds the correct indexes to paginate dataset.
         """
-        assert type(page) == int
-        assert type(page_size) == int
         assert page > 0
         assert page_size > 0
-        csv_size = len(self.dataset())
         start, end = index_range(page, page_size)
-        end = min(end, csv_size)
-        if start >= csv_size:
+        end = min(end, len(self.dataset()))
+        if start >= len(self.dataset()):
             return []
         return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1,
+                  page_size: int = 10) -> Dict[str, Any]:
+        """ Returns a dictionary.
+        """
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page + 1 <= total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Returns a tuple containing a start and end index.
     """
     return ((page - 1) * page_size, page * page_size)
+
